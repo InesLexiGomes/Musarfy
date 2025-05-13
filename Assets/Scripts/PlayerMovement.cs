41a4b9f7a5cab63a1a846923253a9e96ed6eac0b
Inês Gomes <inesgomes.pessoal05@gmail.com>
2025-05-13 13:49:19 +0100

    Started working on the character movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationMultiplier;
+
+    private Rigidbody2D rb;
+
+    private float currentAngle;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        DoMovement();
+    }
+
+    private void DoMovement()
+    {
+        Vector2 moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.linearVelocity = moveDirection * speed;
+        Rotate(moveDirection);
+    }
+
+    private void Rotate(Vector2 moveDirection)
+    {
+        if (moveDirection !=  Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg + -90;
+            
+            currentAngle = Mathf.Lerp(currentAngle, targetAngle, Time.deltaTime * rotationMultiplier);
+            transform.rotation = Quaternion.AngleAxis(currentAngle, Vector3.forward);
+        }
+    }
+}
