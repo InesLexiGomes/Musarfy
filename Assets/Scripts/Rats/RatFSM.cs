0b5dc0284174829cfd8155c62cf93f8a1feb2624
Inês Gomes <inesgomes.pessoal05@gmail.com>
2025-05-15 12:29:25 +0100

    Started working on the condition and action methods

--- Assets/Scripts/Rats/RatFSM.cs
+++ Assets/Scripts/Rats/RatFSM.cs
@@ -2,13 +2,44 @@ using UnityEngine;
 
 public class RatFSM : FSM
 {
+    [SerializeField] private float maxDistance;
+    [SerializeField] private float returnDistance;
+
+    private PlayerInput player;
+
+    private RatMovement movement;
+
+    private bool requirementsFulfilled = false;
+    private bool beingInteractedWith = false;
+    private bool isSpotted = false;
+
+    private void Start()
+    {
+        player = FindAnyObjectByType<PlayerInput>();
+    }
+
     public override void DoAction(string actionID)
     {
         switch (actionID)
         {
+            case ("Follow"):
+                Follow();
+                break;
             
+            case ("Reset"):
+                Reset();
+                break;
+            
+            case ("Return"):
+                Return();
+                break;
+            
+            case ("Talk"):
+                Talk();
+                break;
+
             default:
-                return;
+                break;
         }
     }
 
@@ -16,9 +47,59 @@ public class RatFSM : FSM
     {
         switch (conditionID)
         {
+            case ("Interacting?"):
+                return beingInteractedWith;
 
+            case ("Requirements Fulfilled?"):
+                return requirementsFulfilled;
+
+            case ("Is Spotted?"):
+                return isSpotted;
+            
+            case ("Out of Range?"):
+                return OutOfPlayerRange();
+            
             default:
                 return false;
         }
+    }
+
+    // Action Methods
+    private void Follow()
+    {
+        Vector2 moveDirection = (transform.position - player.transform.position).normalized;
+        movement.DoMovement(moveDirection);
+    }
+    private void Reset()
+    {
+
+    }
+    private void Return()
+    {
+        Vector2 newPosition = -(transform.position - player.transform.position).normalized * returnDistance;
+        transform.position = newPosition;
+    }
+    private void Talk()
+    {
+
+    }
+
+    // Condition Methods
+    public void Interacting()
+    {
+        beingInteractedWith = true;
+    }
+    public void FulfillRequirements()
+    {
+        requirementsFulfilled = true;
+    }
+    public void Seen()
+    {
+        isSpotted = true;
+    }
+    private bool OutOfPlayerRange()
+    {
+        float distance = (transform.position - player.transform.position).magnitude;
+        return distance > maxDistance;
     }
 }
